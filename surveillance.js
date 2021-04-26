6ad842f4dfe207088f747483250856a8415f71a4
Tony Luo <tonyluo2001@gmail.com>
2021-04-26 00:29:08 -0400

    create & implement analog class

--- surveillance.js
+++ surveillance.js
@@ -5,7 +5,7 @@ var ipInput = document.getElementsByClassName("ip");
 var analogInput = document.getElementsByClassName("analog");
 
 
-// NvrConfig class to process IP recorder calculation)
+// NvrConfig class to process IP recorder calculation
 class NvrConfig {
     constructor(qty) {
         this.initializeIP(qty);
@@ -61,13 +61,49 @@ class NvrConfig {
 
 }
 
+// DvrCifig class to process analog recorder calculation
+class DvrConfig {
+    constructor(qty) {
+        this.initializeAnalog(qty);
+    }
 
+    initializeAnalog(qty) {
+        this.camQty = qty;
+        this.dvr8ch = 0;
+        this.dvr16ch = 0;
+        this.dvr32ch = 0;
+        this.pb9ch = 0;
+        this.pb18ch = 0;
+    }
 
-// var dvr8ch = document.getElementById("dvr8ch");
-// var dvr16ch = document.getElementById("dvr16ch");
-// var dvr32ch = document.getElementById("dvr32ch");
-// var pb9ch = document.getElementById("pb9ch");
-// var pb18ch = document.getElementById("pb18ch");
+    calculateAnalog() {
+        this.initializeAnalog(this.camQty);
+        if (this.camQty <= 8) {
+            this.dvr8ch = 1;
+            if (this.camQty <= 4) {
+                this.pb9ch = 1;
+            } else {
+                this.pb18ch = 1;
+            }
+        } else if (this.camQty <= 16) {
+            this.dvr16ch = 1;
+            this.pb18ch = 1;
+        } else {
+            this.dvr32ch = 1;
+            this.pb18ch = 2;
+        }
+        this.writeToHtml();
+    }
+
+    writeToHtml() {
+        document.getElementById("dvr8ch").value = this.dvr8ch;
+        document.getElementById("dvr16ch").value = this.dvr16ch;
+        document.getElementById("dvr32ch").value = this.dvr32ch;
+        document.getElementById("pb9ch").value = this.pb9ch;
+        document.getElementById("pb18ch").value = this.pb18ch;
+    }
+
+}
 
 var mon22in = document.getElementById("mon22in");
 var mon40in = document.getElementById("mon40in");
@@ -77,8 +113,8 @@ var hdmiExt = document.getElementById("hdmiExt");
 function selectSurveillance() {
     surveillance = document.getElementById("surveillance");
     text = document.getElementById("camSelection");
-    camQty = parseInt(document.getElementById("camQty").value);
-    console.log(camQty);
+    // camQty = parseInt(document.getElementById("camQty").value);
+    // console.log(camQty);
     ipInput = document.getElementsByClassName("ip");    //a group of input for ip devices
     // console.log(ipInput);
     analogInput = document.getElementsByClassName("analog");    //a group of input for analog devices
@@ -109,12 +145,13 @@ function calculateRecorder() {
         nvrConfig.calculateIP();
         // calculateIP(camQty);
     } else if (surveillance.value == "Analog Camera") {
-        calculateAnalog(camQty);
+        var dvrConfig = new DvrConfig(camQty);
+        dvrConfig.calculateAnalog();
     }
 
 }
 
-
+// block inputs of either IP or analog system based on selection
 function disableInput(inputGroup) {
     for (var i = 0; i < inputGroup.length; i++) {
         inputGroup[i].value = 0;
@@ -122,6 +159,7 @@ function disableInput(inputGroup) {
     }
 }
 
+// enable inputs of either IP or analog system based on selection
 function enableInput(inputGroup) {
     for (var i = 0; i < inputGroup.length; i++) {
         inputGroup[i].value = 0;
