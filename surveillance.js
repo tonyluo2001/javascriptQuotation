e4cea694f6b3138c5b250eb197da578180d39f8d
Tony Luo <tonyluo2001@gmail.com>
2021-04-27 01:24:57 -0400

    implementing dvr calculator

--- surveillance.js
+++ surveillance.js
@@ -1,122 +1,113 @@
 // NvrConfig class to process IP recorder calculation
 class NvrConfig {
-    constructor(qty) {
-        this.initializeIP(qty);
-        
+    constructor() {
+        this.camQty = document.getElementById("camQty");
+        this.nvr8ch = document.getElementById("nvr8ch");
+        this.nvr16ch = document.getElementById("nvr16ch");
+        this.nvr32ch = document.getElementById("nvr32ch");
+        this.nvr64ch = document.getElementById("nvr64ch");
+        this.poe16ch = document.getElementById("poe16ch");
+        this.poe24ch = document.getElementById("poe24ch");
+        this.initializeIP();
     }
 
-    initializeIP(qty) {
-        this.camQty = qty;
-        this.nvr8ch = 0;
-        this.nvr16ch = 0;
-        this.nvr32ch = 0;
-        this.nvr64ch = 0;
-        this.poe16ch = 0;
-        this.poe24ch = 0;
+    initializeIP() {
+        this.nvr8ch.setAttribute("value", 0);
+        this.nvr16ch.setAttribute("value", 0);
+        this.nvr32ch.setAttribute("value", 0);
+        this.nvr64ch.setAttribute("value", 0);
+        this.poe16ch.setAttribute("value", 0);
+        this.poe24ch.setAttribute("value", 0);
+
     }
 
-    calculateIP(qty) {
-        this.initializeIP(qty);
+    calculateIP() {
+        this.initializeIP();
         // console.log("IP Camera Quantity: " + this.camQty);
-        if (this.camQty <= 8) {
-            this.nvr8ch = 1;
-        } else if (this.camQty <= 16) {
-            this.nvr16ch = 1;
-        } else if (this.camQty <= 32) {
-            this.nvr32ch = 1;
-            if (this.camQty <= 24) {
-                this.poe24ch = 1;
+        if (this.camQty.value <= 8) {
+            this.nvr8ch.setAttribute("value", 1);
+        } else if (this.camQty.value <= 16) {
+            this.nvr16ch.setAttribute("value", 1);
+        } else if (this.camQty.value <= 32) {
+            this.nvr32ch.setAttribute("value", 1);
+            if (this.camQty.value <= 24) {
+                this.poe24ch.setAttribute("value", 1);
             } else {
-                this.poe16ch = 2;
+                this.poe16ch.setAttribute("value", 2);
             }
         } else {
-            this.nvr64ch = 1;
-            if (this.camQty <= 40) {
-                this.poe16ch = 1;
-                this.poe24ch = 1;
-            } else if (this.camQty <= 48) {
-                this.poe24ch = 2;
+            this.nvr64ch.setAttribute("value", 1);
+            if (this.camQty.value <= 40) {
+                this.poe16ch.setAttribute("value", 1);
+                this.poe24ch.setAttribute("value", 1);
+            } else if (this.camQty.value <= 48) {
+                this.poe24ch.setAttribute("value", 2);
             } else {
-                this.poe16ch = 1;
-                this.poe24ch = 2;
+                this.poe16ch.setAttribute("value", 1);
+                this.poe24ch.setAttribute("value", 2);
             }
         }
-        this.writeToHtml();
-    }
-
-    writeToHtml() {
-        document.getElementById("nvr8ch").value = this.nvr8ch;
-        document.getElementById("nvr16ch").value = this.nvr16ch;
-        document.getElementById("nvr32ch").value = this.nvr32ch;
-        document.getElementById("nvr64ch").value = this.nvr64ch;
-        document.getElementById("poe16ch").value = this.poe16ch;
-        document.getElementById("poe24ch").value = this.poe24ch;
-        document.getElementById("hd4tb").value = this.nvr8ch;
-        document.getElementById("hd8tb").value = this.nvr16ch + this.nvr32ch * 2 + this.nvr64ch * 4;
+        hd4tb.setAttribute("value", parseInt(this.nvr8ch.value));
+        hd8tb.setAttribute("value", parseInt(this.nvr16ch.value) + parseInt(this.nvr32ch.value) * 2 + parseInt(this.nvr64ch.value) * 4);
     }
 
 }
 
 // DvrCifig class to process analog recorder calculation
 class DvrConfig {
-    constructor(qty) {
-        this.initializeAnalog(qty);
+    constructor() {
+        this.camQty = document.getElementById("camQty");
+        this.dvr8ch = document.getElementById("dvr8ch");
+        this.dvr16ch = document.getElementById("dvr16ch");
+        this.dvr32ch = document.getElementById("dvr32ch");
+        this.pb9ch = document.getElementById("pb9ch");
+        this.pb18ch = document.getElementById("pb18ch");
+        this.initializeAnalog();
     }
 
-    initializeAnalog(qty) {
-        this.camQty = qty;
-        this.dvr8ch = 0;
-        this.dvr16ch = 0;
-        this.dvr32ch = 0;
-        this.pb9ch = 0;
-        this.pb18ch = 0;
+    initializeAnalog() {
+        this.dvr8ch.setAttribute("value", 0);
+        this.dvr16ch.setAttribute("value", 0);
+        this.dvr32ch.setAttribute("value", 0);
+        this.pb9ch.setAttribute("value", 0);
+        this.pb18ch.setAttribute("value", 0);
     }
 
-    calculateAnalog(qty) {
-        this.initializeAnalog(qty);
-        if (this.camQty <= 8) {
-            this.dvr8ch = 1;
-            if (this.camQty <= 4) {
-                this.pb9ch = 1;
+    calculateAnalog() {
+        this.initializeAnalog();
+        if (this.camQty.value <= 8) {
+            this.dvr8ch.setAttribute("value", 1);
+            if (this.camQty.value <= 4) {
+                this.pb9ch.setAttribute("value", 1);
             } else {
-                this.pb18ch = 1;
+                this.pb18ch.setAttribute("value", 1);
             }
-        } else if (this.camQty <= 16) {
-            this.dvr16ch = 1;
-            this.pb18ch = 1;
+        } else if (this.camQty.value <= 16) {
+            this.dvr16ch.setAttribute("value", 1);
+            this.pb18ch.setAttribute("value", 1);
         } else {
-            this.dvr32ch = 1;
-            this.pb18ch = 2;
+            this.dvr32ch.setAttribute("value", 1);
+            this.pb18ch.setAttribute("value", 2);
         }
-        this.writeToHtml();
-    }
-
-    writeToHtml() {
-        document.getElementById("dvr8ch").value = this.dvr8ch;
-        document.getElementById("dvr16ch").value = this.dvr16ch;
-        document.getElementById("dvr32ch").value = this.dvr32ch;
-        document.getElementById("pb9ch").value = this.pb9ch;
-        document.getElementById("pb18ch").value = this.pb18ch;
-        document.getElementById("hd2tb").value = this.dvr8ch;
-        document.getElementById("hd4tb").value = this.dvr16ch;
-        document.getElementById("hd8tb").value = this.dvr32ch;
     }
 
 }
-
 
 var surveillance;
 var text;
 var camQty = 1;
 var ipInput;
 var analogInput;
-var nvrConfig = new NvrConfig(camQty);
-var dvrConfig = new DvrConfig(camQty);
+var nvrConfig = new NvrConfig();
+var dvrConfig = new DvrConfig();
+
+var hd2tb = document.getElementById("hd2tb");
+var hd4tb = document.getElementById("hd4tb");
+var hd8tb = document.getElementById("hd8tb");
 
 var mon22in = document.getElementById("mon22in");
 var mon40in = document.getElementById("mon40in");
 var hdmiExt = document.getElementById("hdmiExt");
-
 
 function selectSurveillance() {
     surveillance = document.getElementById("surveillance");
@@ -128,19 +119,17 @@ function selectSurveillance() {
     analogInput = document.getElementsByClassName("analog");    //a group of input for analog devices
     // console.log(analogInput);
 
-    document.getElementById("hd2tb").value = 0;
-    document.getElementById("hd4tb").value = 0;
-    document.getElementById("hd8tb").value = 0;
-
     // console.log(surveillance.value);
     if (surveillance.value == "IP Camera") {
         text.innerHTML = "Quoting IP System";
         disableInput(analogInput);
         enableInput(ipInput);
+        nvrConfig = new NvrConfig();
     } else if (surveillance.value == "Analog Camera") {
         text.innerHTML = "Quoting Analog System";
         disableInput(ipInput);
         enableInput(analogInput);
+        dvrConfig = new DvrConfig();
     } else {
         disableInput(ipInput);
         disableInput(analogInput);
@@ -154,11 +143,11 @@ function calculateRecorder() {
     // console.log(surveillance.value);
 
     if (surveillance.value == "IP Camera") {
-        nvrConfig.calculateIP(camQty);
+        nvrConfig.calculateIP();
 
         // calculateIP(camQty);
     } else if (surveillance.value == "Analog Camera") {
-        dvrConfig.calculateAnalog(camQty);
+        dvrConfig.calculateAnalog();
     }
 
 }
@@ -169,6 +158,9 @@ function disableInput(inputGroup) {
         inputGroup[i].value = 0;
         inputGroup[i].disabled = true;
     }
+    hd2tb.setAttribute("value", 0);
+    hd4tb.setAttribute("value", 0);
+    hd8tb.setAttribute("value", 0);
 }
 
 // enable inputs of either IP or analog system based on selection
@@ -182,11 +174,11 @@ function enableInput(inputGroup) {
 function printTables() {
     surveillance = document.getElementById("surveillance");
     if (surveillance.value == "IP Camera") {
-        nvrConfig.calculateIP(camQty);
+        nvrConfig.calculateIP();
 
         // calculateIP(camQty);
     } else if (surveillance.value == "Analog Camera") {
-        dvrConfig.calculateAnalog(camQty);
+        dvrConfig.calculateAnalog();
     }
 
 
